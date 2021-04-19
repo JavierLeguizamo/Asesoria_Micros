3e8a2cd1d3f646f9f6ede6f91b2894ede5d24de7
Javier Leguízamo <javier.leguizamo.ie.tesvg@gmail.com>
2021-04-19 12:35:36 -0500

    Display 7 Segmentos

---
+++ Display_7_Seg/Display_7_Seg/main.c
@@ -0,0 +1,28 @@
+
+#define F_CPU 1e6
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define DIRECCION_PB DDRB
+#define TODOS_BITS_SALIDAS 0xFF
+#define ESCRIBIR_DISPLAY(VALOR) PORTB = VALOR
+#define CERO 0x3F
+#define UNO 0x06
+#define TWO 0X5B
+
+unsigned char numero[]={CERO,UNO,TWO};
+
+int main(void)
+{
+    DIRECCION_PB |= TODOS_BITS_SALIDAS;
+    while (1) 
+    {
+		for (int i = 0; i<=2; i++)
+		{
+			ESCRIBIR_DISPLAY(numero[i]);
+			_delay_ms(500);
+		}
+    }
+}
+
+
